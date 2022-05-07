8797c30e470ee1c7b009a398eb5c0ebe9cfd83a2
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-05-07 22:31:22 +0300

    init

---
+++ app/src/main/java/com/kotlinweatherapp/utilities/Constants.kt
@@ -0,0 +1,9 @@
+package com.kotlinweatherapp.utilities
+
+class Constants {
+    companion object {
+        const val BASE_URL: String = "https://api.openweathermap.org"
+        const val API_KEY: String = ""
+        const val UNITS: String = "metric"
+    }
+}
