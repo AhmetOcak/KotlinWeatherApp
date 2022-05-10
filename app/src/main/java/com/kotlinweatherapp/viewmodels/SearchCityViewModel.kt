0649ab48f47d0d0a8e115e3bac09fee8f89eecd5
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-05-10 23:25:59 +0300

    created

---
+++ app/src/main/java/com/kotlinweatherapp/viewmodels/SearchCityViewModel.kt
@@ -0,0 +1,17 @@
+package com.kotlinweatherapp.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+class SearchCityViewModel: ViewModel() {
+
+    val currentText = MutableLiveData<String>()
+
+    var isCanGo = Transformations.map(currentText) {
+        !currentText.value.isNullOrEmpty()
+    }
+
+
+
+}
