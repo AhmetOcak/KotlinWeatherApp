10d652b072b99fc4a3319035df3944bb9cd41d4c
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-05-10 23:25:49 +0300

    events added

--- app/src/main/java/com/kotlinweatherapp/view/WeatherFragment.kt
+++ app/src/main/java/com/kotlinweatherapp/view/WeatherFragment.kt
@@ -5,27 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.kotlinweatherapp.R
 import com.kotlinweatherapp.databinding.FragmentWeatherBinding
 import com.kotlinweatherapp.viewmodels.WeatherViewModel
+import com.kotlinweatherapp.viewmodels.WeatherViewModelFactory
 
 
 class WeatherFragment : Fragment() {
 
-    private val viewModel: WeatherViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var viewModel: WeatherViewModel
+    private lateinit var cityName: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentWeatherBinding.inflate(inflater)
+        cityName = requireArguments().getString("cityName") ?: "Ankara"
+
+        val weatherViewModelFactory =
+            WeatherViewModelFactory(cityName)
+        viewModel = ViewModelProvider(this, weatherViewModelFactory).get(WeatherViewModel::class.java)
+
+
         binding.viewModel = viewModel
-        binding.lifecycleOwner = this
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.searchButton.setOnClickListener {
+            goToNextScreen()
+        }
         return binding.root
     }
 
+    private fun goToNextScreen() {
+        findNavController().navigate(R.id.action_weatherFragment_to_searchCityFragment)
+    }
 }
