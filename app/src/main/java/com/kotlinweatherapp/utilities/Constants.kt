368bad48144e8b4e54b034c79358aacd0bd5a716
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-05-22 14:00:53 +0300

    r

--- app/src/main/java/com/kotlinweatherapp/utilities/Constants.kt
+++ app/src/main/java/com/kotlinweatherapp/utilities/Constants.kt
@@ -3,7 +3,7 @@ package com.kotlinweatherapp.utilities
 class Constants {
     companion object {
         const val BASE_URL: String = "https://api.openweathermap.org"
-        const val API_KEY: String = "18fcba115a2815e21a21379f65b994ed"
+        const val API_KEY: String = ""
         const val UNITS: String = "metric"
         const val PERMISSION_ID = 101
         const val LOCATION_DATA = "locationData"
