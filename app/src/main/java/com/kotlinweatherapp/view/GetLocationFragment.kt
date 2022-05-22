ea8f06f09f587d788f912db4ea5640564100694e
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-05-22 13:51:16 +0300

    location created

---
+++ app/src/main/java/com/kotlinweatherapp/view/GetLocationFragment.kt
@@ -0,0 +1,153 @@
+package com.kotlinweatherapp.view
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.*
+import com.kotlinweatherapp.R
+import com.kotlinweatherapp.data.LocationData
+import com.kotlinweatherapp.databinding.FragmentGetLocationBinding
+import com.kotlinweatherapp.utilities.Constants
+import com.kotlinweatherapp.viewmodels.GetLocationViewModel
+
+class GetLocationFragment : Fragment() {
+
+    private lateinit var binding: FragmentGetLocationBinding
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private val viewModel: GetLocationViewModel by viewModels()
+    private lateinit var locationData: LocationData
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentGetLocationBinding.inflate(inflater)
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        getLastLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.lastLocation.addOnCompleteListener(requireActivity()) { task ->
+                    val location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                        viewModel.setProgressBarVisibility(View.VISIBLE)
+                        getLastLocation()
+                    } else {
+                        viewModel.setProgressBarVisibility(View.VISIBLE)
+                        locationData = LocationData(location.latitude, location.longitude)
+                        goToNextScreen()
+                    }
+                }
+            } else {
+                Toast.makeText(requireContext(), "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        Looper.myLooper()?.let {
+            mFusedLocationClient.requestLocationUpdates(
+                mLocationRequest, mLocationCallback,
+                it
+            )
+        }
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            Constants.PERMISSION_ID
+        )
+        getLastLocation()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == Constants.PERMISSION_ID) {
+            if (!(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Log.e("e", "request denied")
+            }
+        }
+    }
+
+    private fun goToNextScreen() {
+        findNavController().navigate(
+            R.id.action_getLocationFragment_to_weatherFragment,
+            Bundle().apply { putSerializable(Constants.LOCATION_DATA, locationData) })
+    }
+}
---
+++ app/src/main/java/com/kotlinweatherapp/viewmodels/GetLocationViewModel.kt
@@ -0,0 +1,16 @@
+package com.kotlinweatherapp.viewmodels
+
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GetLocationViewModel: ViewModel() {
+
+    private val _progressBarVisibility = MutableLiveData(View.GONE)
+    val progressBarVisibility: LiveData<Int> get() = _progressBarVisibility
+
+    fun setProgressBarVisibility(visibility: Int) {
+        _progressBarVisibility.value = visibility
+    }
+}
