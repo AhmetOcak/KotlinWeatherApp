f7328f94ccca1c53f57582849f05f0356185310a
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-05-31 14:20:15 +0300

    Hilt | Module added

---
+++ app/src/main/java/com/kotlinweatherapp/di/LocationModule.kt
@@ -0,0 +1,19 @@
+package com.kotlinweatherapp.di
+
+import android.app.Application
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+object LocationModule {
+
+    @Provides
+    fun provideFusedLocationProviderClient(application: Application): FusedLocationProviderClient {
+        return LocationServices.getFusedLocationProviderClient(application)
+    }
+}
