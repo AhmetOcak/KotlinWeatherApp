88a9790a38c39165404052d7b59fa6dc1ff7fdb0
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-07-03 22:13:43 +0300

    weather entity created

--- app/src/main/java/com/kotlinweatherapp/data/remote/response/WeatherEntity.kt
+++ app/src/main/java/com/kotlinweatherapp/data/remote/response/WeatherEntity.kt
@@ -1,4 +1,57 @@
 package com.kotlinweatherapp.data.remote.response
 
-class WeatherEntity {
-}
+import com.google.gson.annotations.SerializedName
+
+data class WeatherEntity(
+
+    @SerializedName("main")
+    val weatherData: MainE,
+
+    @SerializedName("wind")
+    val windSpeed: WindE,
+
+    @SerializedName("weather")
+    val weatherStatus: List<WeatherE>,
+
+    @SerializedName("sys")
+    val sunTimes: SysE,
+
+    @SerializedName("name")
+    val cityName: String,
+)
+
+data class MainE(
+
+    @SerializedName("temp")
+    val temp: Double,
+
+    @SerializedName("feels_like")
+    val feelsLike: Double,
+
+    @SerializedName("pressure")
+    val pressure: Double,
+
+    @SerializedName("humidity")
+    val humidity: Int,
+)
+
+data class WindE(
+
+    @SerializedName("speed")
+    val speed: Double,
+)
+
+data class SysE(
+
+    @SerializedName("sunrise")
+    val sunrise: Long,
+
+    @SerializedName("sunset")
+    val sunset: Long,
+)
+
+data class WeatherE(
+
+    @SerializedName("description")
+    val description: String,
+)
